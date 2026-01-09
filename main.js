394ee6c1d62b54de9c2795b490d94caac34db396
Gaëtan H <me@gaetanhus.fr>
2026-01-09 10:48:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1100,
@@ -26,3 +27,4 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
+
